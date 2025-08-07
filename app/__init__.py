2c9db46889783ef35062f302b67a356b21d07336
Pratham Agrawal <prathamagrawal785@gmail.com>
2025-08-07 20:19:44 +0530

    Add persistent session using Flask-Session

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_session import Session
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    app.secret_key = "your_secret_key"
+    app.config['SECRET_KEY'] = 'your-secret'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SESSION_TYPE'] = 'filesystem'  # Persistent storage
+    app.config['SESSION_PERMANENT'] = False
+    
+    Session(app)
 
     db.init_app(app)
 
